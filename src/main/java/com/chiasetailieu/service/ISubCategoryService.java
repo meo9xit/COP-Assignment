e90b311eb4fdde64ef9c240575f8b0b4bd0cc340
thanhchu98 <55565902+thanhchu98@users.noreply.github.com>
2019-10-26 11:39:53 +0700

    add file hha

---
+++ src/main/java/com/chiasetailieu/service/ISubCategoryService.java
@@ -0,0 +1,17 @@
+package com.chiasetailieu.service;
+
+import java.util.List;
+
+import com.chiasetailieu.model.Category;
+import com.chiasetailieu.model.Document;
+import com.chiasetailieu.model.SubCategory;
+
+
+
+public interface ISubCategoryService {
+	SubCategory save(SubCategory subcategory);
+	List<SubCategory> findAll();
+	void delete(SubCategory subcategory);
+	SubCategory findByCategories(Category category);
+	SubCategory findOneByDocument(Document document);
+}
