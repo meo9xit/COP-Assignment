90c1afdef1275553441afbea9b8c25773ae75bb2
thanhchu98 <55565902+thanhchu98@users.noreply.github.com>
2019-10-28 09:03:41 +0700

    AdminSubCategory.java

---
+++ src/main/java/com/chiasetailieu/api/SubCategoryAPI.java
@@ -0,0 +1,64 @@
+package com.chiasetailieu.api;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.chiasetailieu.model.SubCategory;
+import com.chiasetailieu.service.ISubCategoryService;
+import com.chiasetailieu.utils.HttpUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Servlet implementation class UserAPI
+ */
+@WebServlet(urlPatterns = "/api-admin-subcategory")
+public class SubCategoryAPI extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	
+	@Inject
+	ISubCategoryService subcategoryService;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		ObjectMapper mapper = new ObjectMapper();
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("application/json");
+		SubCategory subcategory =  HttpUtil.of(request.getReader()).toModel(SubCategory.class);
+		subcategory = subcategoryService.save(subcategory);
+		mapper.writeValue(response.getOutputStream(), subcategory);
+	}
+	
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+	}
+	
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("application/json");
+		SubCategory subcategory =  HttpUtil.of(request.getReader()).toModel(SubCategory.class);
+		subcategoryService.delete(subcategoryService.findByCategories(subcategory.getCategory()));
+		mapper.writeValue(response.getOutputStream(), "{}");
+	}
+
+}
---
+++ src/main/java/com/chiasetailieu/service/impl/SubCategoryService.java
@@ -0,0 +1,48 @@
+package com.chiasetailieu.service.impl;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import com.chiasetailieu.dao.ISubcategoryDAO;
+import com.chiasetailieu.model.Category;
+import com.chiasetailieu.model.Document;
+import com.chiasetailieu.model.SubCategory;
+import com.chiasetailieu.service.ISubCategoryService;
+
+public class SubCategoryService implements ISubCategoryService{
+	
+	@Inject
+	ISubcategoryDAO SubCategoryDAO;
+
+	@Override
+	public SubCategory save(SubCategory subcategory) {
+		Long newID = SubCategoryDAO.save(subcategory);
+		return SubCategoryDAO.findOneById(newID);
+	}
+
+	@Override
+	public List<SubCategory> findAll() {
+		return SubCategoryDAO.findAll();
+	}
+
+	@Override
+	public void delete(SubCategory subcategory) {
+		SubCategoryDAO.delete(subcategory);
+		
+	}
+
+	@Override
+	public SubCategory findByCategories(Category category) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SubCategory findOneByDocument(Document document) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
