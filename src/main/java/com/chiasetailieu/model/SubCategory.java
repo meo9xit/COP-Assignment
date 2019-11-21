49110faeb74187ee704ff55ede006ebbe79e5fc1
thanhchu98 <55565902+thanhchu98@users.noreply.github.com>
2019-11-21 16:29:55 +0700

    Update SubCategory.java

--- src/main/java/com/chiasetailieu/model/SubCategory.java
+++ src/main/java/com/chiasetailieu/model/SubCategory.java
@@ -5,29 +5,37 @@ public class SubCategory extends AbstractModel<SubCategory> {
 	private String subcategoryName;
 	private Category category;
 	private long categoryId;
+
 	public Long getSubcategoryID() {
 		return subcategoryID;
 	}
+
 	public void setSubcategoryID(Long subcategoryID) {
 		this.subcategoryID = subcategoryID;
 	}
+
 	public String getSubcategoryName() {
 		return subcategoryName;
 	}
+
 	public void setSubcategoryName(String subcategoryName) {
 		this.subcategoryName = subcategoryName;
 	}
+
 	public Category getCategory() {
 		return category;
 	}
+
 	public void setCategory(Category category) {
 		this.category = category;
 	}
+
 	public long getCategoryId() {
 		return categoryId;
 	}
+
 	public void setCategoryId(long l) {
 		this.categoryId = l;
 	}
-	
+
 }
