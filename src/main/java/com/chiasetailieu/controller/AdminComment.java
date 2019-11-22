4641666a4e10829bf0b463a66aa680429d647c27
thanhchu98 <55565902+thanhchu98@users.noreply.github.com>
2019-11-22 09:53:22 +0700

    Update AdminComment.java

--- src/main/java/com/chiasetailieu/controller/AdminComment.java
+++ src/main/java/com/chiasetailieu/controller/AdminComment.java
@@ -14,7 +14,7 @@ import com.chiasetailieu.model.Comment;
 import com.chiasetailieu.service.ICommentService;
 import com.chiasetailieu.utils.FormUtil;
 
-@WebServlet("/admin-comment")
+@WebServlet(urlPatterns = "/admin-comment")
 public class AdminComment extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
